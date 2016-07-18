f233893c76e62def882fdb22672813311a864a24
engrsafwan <eng.safu@gmail.com>
2016-07-18 11:58:47 +0500

    Sixth Code

--- merging_string.py
+++ merging_string.py
@@ -2,3 +2,9 @@ first_name = "ada"
 last_name = "lovelace"
 full_name = first_name + " " + last_name
 print(full_name)
+
+
+first_name = "Muhammad"
+last_name = "Safwan"
+full_name = first_name + " " + last_name
+print("Hello, " + full_name.title() + "!")
